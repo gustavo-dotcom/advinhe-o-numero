8b6e41f9d623ccbb052ff4bbbe1939c52d4bc134
gustavo-dotcom <gustavo.oliveira2003@hotmail.com>
2023-08-11 14:41:43 -0300

    adicionando a logica do programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿Random random = new Random();
 int secretNumber = random.Next(1, 100);
+
+do
+{
+    Console.WriteLine("Type a number between 1 and 100: ");
+    int guess = int.Parse(Console.ReadLine());
+
+    if (guess == secretNumber)
+    {
+        Console.WriteLine("Congratuations! You've guessed it");    
+    }
+    else if (guess > secretNumber)
+    {
+        Console.WriteLine("Guess lower");
+    }
+    else
+    {
+        Console.WriteLine("Guess bigger");
+    }
+} 
+while (true);
